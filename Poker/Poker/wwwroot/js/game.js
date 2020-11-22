6159bc951e68022fda99f4c7929a2c263160b5be
Ajay Bagali <ajaybagali@gmail.com>
2020-11-21 18:26:36 -0700

    added background + removed privacy links

---
+++ Poker/Poker/wwwroot/js/game.js
@@ -0,0 +1,29 @@
+﻿/**
+ * Initialize the WebCanvas Pixi Stage
+ *
+ * Author: H. James de St. Germain
+ * Date: Fall 2019
+ *
+ */
+
+/**
+ * Global Variable - access to the PIXI Application
+ */
+class Game
+{
+
+    constructor() { }
+/**
+ *  Create PIXI stage
+ *  @param {int} width  - how wide the stage
+ *  @param {int} height - how high the stage
+ *  @param {int} bg_color - background color of the stage
+ *
+ */
+    setup_pixi_stage(width, height, bg_color)
+    {
+        var app = new PIXI.Application({ backgroundColor: bg_color });
+        app.renderer.resize(width, height);
+        $("#canvas_div").append(app.view);
+    }
+}
